ffc3e74844aabe43440e9317a68c0cca2a59d5a6
Marek Hrusovsky <xhruso00@gmail.com>
2014-08-22 13:02:49 +0200

    Initial Commit

---
+++ MHDecoratorExamples/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MHDecoratorExamples
+//
+//  Created by Marek Hrusovsky on 22/08/14.
+//  Copyright (c) 2014 Marek Hrusovsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
